7c04aef9c725f597f3f156f7af84136c19d3c63b
ruhul <ruhulaminhirai63@gmail.com>
2024-02-10 02:11:50 +0600

    fetched all user and showed to each card

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
+import { Button } from './ui/button';
+
+const UserCard = ({ user }) => {
+  let userName = user.firstName + " " + user.lastName; 
+  return (
+    <div className='text-center cursor-pointer'>
+      <Card className="relative overflow-hidden custom__hover-card">
+        <CardHeader className="pb-0">
+          <div className='flex justify-center pb-2'>
+            <img className='max-w-40' src={user.image} alt={userName} loading="lazy" />
+          </div>
+          <p>{user.lastName}</p>
+          <CardDescription>{userName}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>{user.email}</p>
+        </CardContent>
+        <div className='custom__hover-view absolute bg-gray-800 inset-0 p-2 flex flex-col justify-center items-center'>
+          <p className='font-medium'>{userName}</p>
+          <p>{user.email}</p>
+          <p>{user.address.address}</p>
+          <p>{user.address.city + ", " + user.address.state}</p>
+          <p>{user.company.name}</p>
+          <Button variant="outline" className="mt-3">View more</Button>
+        </div>
+      </Card>
+
+    </div>
+  )
+}
+
+export default UserCard
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,52 @@
-import HeroSection from "@/components/HeroSection"
-
+import AddUser from "@/components/AddUser";
+import HeroSection from "@/components/HeroSection";
+import UserCard from "@/components/UserCard";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    //Fetching all the user
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          setLoading(false);
+          console.log(data);
+          setUser(data.users);
+          setError(false);
+        }
+      })
+      .catch((e) => {
+        setLoading(false);
+        setError(true);
+      });
+  }, []);
+
+  const renderUsers = () => {
+    return (
+      <div className="py-8 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+        {" "}
+        {user &&
+          user.map((el) => {
+            return <UserCard user={el} key={el.id} />;
+          })}
+      </div>
+    );
+  };
+
   return (
     <div>
-        <HeroSection />
+      <HeroSection />
+      <AddUser />
+      {loading ? <p>Loading...</p> : ""}
+      {error ? <p>Error. </p> : ""}
+      {renderUsers()}
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
